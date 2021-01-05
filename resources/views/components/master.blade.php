e684d344468962dbba166415e448c7068225f577
alexreed22496 <areed22496@gmail.com>
2021-01-05 11:27:42 +0000

    welcome view changes

--- resources/views/components/master.blade.php
+++ resources/views/components/master.blade.php
@@ -1,44 +1,21 @@
-<!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+<x-head>
+        <div id="app">
+            <section class="px-14 py-5 mb-4 flex justify-between">
+                <header class="container mx-auto flex flex-row">
+                    <h1 class="w-20">
+                        <img 
+                            src="/images/logo.svg" 
+                            alt="ClayPigeon"
+                        >
+                    </h1>
 
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
+                    <p class="h-auto w-80 pl-7 text-2xl font-bold flex flex-wrap content-center">
+                        Clay Pigeon
+                    </p> 
+                </header>
+            </section>
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+            {{ $slot }}
+        </div>
+</x-head>
 
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
-
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
-
-    <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-</head>
-<body>
-    <div id="app">
-        <section class="px-14 py-5 mb-4 flex justify-between">
-            <header class="container mx-auto flex flex-row">
-                <h1 class="w-20">
-                    <img 
-                        src="/images/logo.svg" 
-                        alt="ClayPigeon"
-                    >
-                </h1>
-
-                <p class="h-auto w-80 pl-7 text-2xl font-bold flex flex-wrap content-center">
-                    Clay Pigeon
-                </p> 
-            </header>
-        </section>
-
-        {{ $slot }}
-    </div>
-
-    <script src="http://unpkg.com/turbolinks"></script>
-</body>
-</html>
